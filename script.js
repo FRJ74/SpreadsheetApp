3724c46f9a259408cd4903536b88935ab50f15c8
FRJ74 <frankr74@gmail.com>
2025-01-29 21:10:14 -0600

    Declare an average function which takes an array of numbers as the nums parameter.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 /* Define the behavior of the window once loaded */
+const isEven = (num) => num % 2 === 0;
+const average = (nums) => sum(nums) / nums.length;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
