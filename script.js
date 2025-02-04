a5b671e49cec2ba030ed1c576699972f26bbd7ea
FRJ74 <frankr74@gmail.com>
2025-02-03 19:45:20 -0600

    Chain the .map() method to your charRange() call

--- script.js
+++ script.js
@@ -22,14 +22,9 @@ const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/ig;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-  const elemValue = (num) => {
-    const inner = (character) => {
-      return idToText(character + num);
-    }; 
-    return inner;
-  };
-  const addCharacters = (character1) => {};
-};
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map();
+
 
 window.onload = () => {
   const container = document.getElementById("container");
